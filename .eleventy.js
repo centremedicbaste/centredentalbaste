6c221e6181501bcd2fa77b8184251f376896d463
Manuel Ruiz Redondo <manuelruizredondo@gmail.com>
2026-07-14 18:06:21 +0200

    Update Eleventy configuration to ignore 'src/interno' directory and add it to passthrough copy. Update robots.txt to disallow crawling of '/interno/' path.

--- .eleventy.js
+++ .eleventy.js
@@ -36,6 +36,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site.webmanifest");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/google5f3d2fed92020926.html");
+  eleventyConfig.ignores.add("src/interno/**");
+  eleventyConfig.addPassthroughCopy("src/interno");
   eleventyConfig.addPassthroughCopy("src/_redirects", "_redirects");
 
   // Netlify CMS
